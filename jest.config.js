e85d533c9a20fa3949b1b69078a71f484d4df7f4
G2D2 <gabriel.g2d2@gmail.com>
2023-01-03 07:25:53 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,33 @@ module.exports = {
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
   },
+  transformIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/?(*.)+(spec|test).(js|jsx|ts|tsx)'],
+  preset: 'ts-jest',
+  clearMocks: true,
+  collectCoverage: true,
   coverageDirectory: 'coverage',
-  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  collectCoverageFrom: [
+    'src/**/*.(js|jsx|ts|tsx)?',
+    '!src/**/*.d.ts',
+    '!src/main.tsx',
+    '!src/global/types/**/*'
+  ],
+  coverageReporters: ['json', 'text', 'lcov', 'clover'],
+  coverageProvider: 'v8',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  maxWorkers: '50%',
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/fileTransformer.js',
-    '\\.css$': 'some-css-transformer',
-    '\\.[jt]sx?$': 'ts-jest'
+      '<rootDir>/fileTransformer.js'
   }
 }
