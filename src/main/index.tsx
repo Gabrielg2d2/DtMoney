13f35778f54d8dd724ebcd40aa6c738fe6442afc
G2D2 <gabriel.g2d2@gmail.com>
2023-01-03 21:26:12 -0300

    fix: react ReactDOM.createRoot to main

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById('main') as HTMLElement)
 
 root.render(
   <React.StrictMode>
