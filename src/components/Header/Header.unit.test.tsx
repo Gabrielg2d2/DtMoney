309fe3675ad35cb5078eb3782e9f59e59f494124
G2D2 <gabriel.g2d2@gmail.com>
2023-01-25 19:06:42 -0300

    test: 💍 component header

---
+++ src/components/Header/Header.unit.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import { Header } from '.'
+
+describe('Header', () => {
+  it('should render component Header', () => {
+    render(<Header />)
+    expect(screen.getByText('dt money')).toBeInTheDocument()
+    expect(screen.getByText('Nova transação')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', {
+        name: /Nova transação/i
+      })
+    ).toBeInTheDocument()
+  })
+})
