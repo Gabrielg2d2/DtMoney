f719257a8f9e0fc3e61406fc988c8da8ed5a6f41
G2D2 <gabriel.g2d2@gmail.com>
2023-01-25 08:43:06 -0300

    test: 💍 should return a new status 201, received by parameter

--- src/data-layer/transaction/use-cases/remove-transaction/remove-transaction.unit.test.ts
+++ src/data-layer/transaction/use-cases/remove-transaction/remove-transaction.unit.test.ts
@@ -10,9 +10,22 @@ describe('RemoveTransaction', () => {
     await sut.removeTransaction(urlDelete)
 
     expect(methodDeleteTransactionSpy).toHaveBeenCalledWith(urlDelete)
+    expect(methodDeleteTransactionSpy).toHaveBeenCalledTimes(1)
   })
 
-  it('should return promise status 200', async () => {
+  it('should call methodDeleteTransaction with correct params', async () => {
+    const { sut, urlSpy, transactionIdSpy } = makeSutDeleteTransaction({
+      statusSpy: 201
+    })
+
+    const urlDelete = `${urlSpy}/${transactionIdSpy}`
+
+    const response = await sut.removeTransaction(urlDelete)
+
+    expect(response.status).toBe(201)
+  })
+
+  it('should return a new status 201, received by parameter', async () => {
     const { sut, urlSpy, transactionIdSpy } = makeSutDeleteTransaction()
 
     const urlDelete = `${urlSpy}/${transactionIdSpy}`
