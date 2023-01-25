ad3a3508ce5236bfe2f62605aae0c659f605ab3a
G2D2 <gabriel.g2d2@gmail.com>
2023-01-25 18:40:28 -0300

    test: 💍 component Card

---
+++ src/components/Card/Card.unit.test.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react'
+import { CardTotal, CardIncoming, CardOutgoing } from '.'
+
+describe('CardTotal', () => {
+  it('should render component CardTotal', () => {
+    render(<CardTotal title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('Card_Jest_Title')).toBeInTheDocument()
+  })
+
+  it('should render component CardTotal with value', () => {
+    render(<CardTotal title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('R$ 0,00')).toBeInTheDocument()
+  })
+})
+
+describe('CardIncoming', () => {
+  it('should render component CardIncoming', () => {
+    render(<CardIncoming title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('Card_Jest_Title')).toBeInTheDocument()
+  })
+
+  it('should render component CardIncoming with value', () => {
+    render(<CardIncoming title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('R$ 0,00')).toBeInTheDocument()
+  })
+})
+
+describe('CardOutgoing', () => {
+  it('should render component CardOutgoing', () => {
+    render(<CardOutgoing title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('Card_Jest_Title')).toBeInTheDocument()
+  })
+
+  it('should render component CardOutgoing with value', () => {
+    render(<CardOutgoing title="Card_Jest_Title" value="R$ 0,00" />)
+
+    expect(screen.getByText('R$ 0,00')).toBeInTheDocument()
+  })
+})
