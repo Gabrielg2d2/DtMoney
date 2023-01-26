38293af2c2d50e45cca826d46719dc01ffce2993
G2D2 <gabriel.g2d2@gmail.com>
2023-01-26 12:51:00 -0300

    chore: config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/react/'
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
