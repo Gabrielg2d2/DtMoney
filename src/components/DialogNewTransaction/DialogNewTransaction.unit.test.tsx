fde75610c7c89d4c2240af8e625c933cdd1728ac
G2D2 <gabriel.g2d2@gmail.com>
2023-01-31 15:52:00 -0300

    test: 💍 component DialogNewTransaction

---
+++ src/components/DialogNewTransaction/DialogNewTransaction.unit.test.tsx
@@ -0,0 +1,88 @@
+import { render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
+import { DialogNewTransaction } from '.'
+
+describe('DialogNewTransaction', () => {
+  it('should render component DialogNewTransaction close, just the button', () => {
+    render(<DialogNewTransaction />)
+
+    expect(screen.getByText(/nova transação/i)).toBeInTheDocument()
+  })
+
+  it('should render component DialogNewTransaction open, with the form', () => {
+    render(<DialogNewTransaction />)
+
+    const buttonOpen = screen.getByText(/nova transação/i)
+
+    act(() => {
+      buttonOpen.click()
+    })
+
+    expect(screen.getByText(/cadastrar transação/i)).toBeInTheDocument()
+    expect(
+      screen.getByText(/insira os dados para nova transação/i)
+    ).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/nome/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/preço/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/categoria/i)).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', {
+        name: /cadastrar/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', {
+        name: /entrada/i
+      })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', {
+        name: /saída/i
+      })
+    ).toBeInTheDocument()
+  })
+
+  it('should allow closing the modal when clicking on the "x" close button', () => {
+    render(<DialogNewTransaction />)
+
+    const buttonOpen = screen.getByText(/nova transação/i)
+
+    act(() => {
+      buttonOpen.click()
+    })
+
+    const buttonClose = screen.getByRole('button', {
+      name: /close dialog/i
+    })
+
+    act(() => {
+      buttonClose.click()
+    })
+
+    expect(screen.queryByText(/cadastrar transação/i)).not.toBeInTheDocument()
+  })
+
+  it('should render component DialogNewTransaction with button withdrawn selection default', () => {
+    render(<DialogNewTransaction />)
+
+    const buttonOpen = screen.getByText(/nova transação/i)
+
+    act(() => {
+      buttonOpen.click()
+    })
+
+    const buttonWithdrawn = screen.getByRole('button', {
+      name: /saída/i
+    })
+
+    const buttonDeposit = screen.getByRole('button', {
+      name: /entrada/i
+    })
+
+    expect(buttonWithdrawn).toHaveClass('border-red-500')
+    expect(buttonDeposit).not.toHaveClass('border-green-500')
+  })
+})
