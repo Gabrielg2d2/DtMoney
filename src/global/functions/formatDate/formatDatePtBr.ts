8dcfb9dd767fa4fc6b3945e2e981a44af7b41e0e
G2D2 <gabriel.g2d2@gmail.com>
2023-06-01 20:18:32 -0300

    refactor: 💡 remove mock

--- src/global/functions/formatDate/formatDatePtBr.ts
+++ src/global/functions/formatDate/formatDatePtBr.ts
@@ -1,17 +1,16 @@
+/**
+ * @description
+ * The date is formatted in pt-BR, and the time zone is set to UTC
+ * @example
+ * 01/04/2021 00:00
+ *
+ * @param {string} date
+ * @returns {string}
+ *
+ *
+ */
 export function formatDatePtBr(date: string): string {
   if (!date) return ''
-
-  /**
-   * @description
-   * The date is formatted in pt-BR, and the time zone is set to UTC
-   * @example
-   * 01/04/2021 00:00
-   *
-   * @param {string} date
-   * @returns {string}
-   *
-   *
-   */
   const formatDate = new Date(date)
   const format = new Intl.DateTimeFormat('pt-BR', {
     year: 'numeric',
