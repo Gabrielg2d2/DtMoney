247ac033212adac536d21f0b31cda22d260bc917
G2D2 <gabriel.g2d2@gmail.com>
2023-06-06 20:27:48 -0300

    test: 💍 fix new transaction

--- src/entity/Transaction/Transaction.unit.test.ts
+++ src/entity/Transaction/Transaction.unit.test.ts
@@ -6,7 +6,7 @@ describe('Transaction', () => {
     const transaction = new Transaction(
       null,
       100,
-      new Date(),
+      new Date().toISOString(),
       'A valid transaction',
       'deposit'
     )
@@ -18,7 +18,7 @@ describe('Transaction', () => {
     const transaction = new Transaction(
       '12345',
       100,
-      new Date(),
+      new Date().toISOString(),
       'A valid transaction',
       'deposit'
     )
@@ -28,17 +28,29 @@ describe('Transaction', () => {
 
   it('should return error, when the value is equal to zero or description is less than 3 letters', () => {
     expect(() => {
-      new Transaction(null, 0, new Date(), 'A valid transaction', 'deposit')
+      new Transaction(
+        null,
+        0,
+        new Date().toISOString(),
+        'A valid transaction',
+        'deposit'
+      )
     }).toThrowError('Invalid transaction')
 
     expect(() => {
-      new Transaction(null, 100, new Date(), 'A', 'deposit')
+      new Transaction(null, 100, new Date().toISOString(), 'A', 'deposit')
     }).toThrowError('Invalid transaction')
   })
 
   it('should return error, when the value is less than zero', () => {
     expect(() => {
-      new Transaction(null, -100, new Date(), 'A valid transaction', 'deposit')
+      new Transaction(
+        null,
+        -100,
+        new Date().toISOString(),
+        'A valid transaction',
+        'deposit'
+      )
     }).toThrowError('Invalid transaction')
   })
 
@@ -46,14 +58,14 @@ describe('Transaction', () => {
     const transaction = new Transaction(
       '12345',
       100,
-      new Date(),
+      new Date('2023-05-01').toISOString(),
       'A valid transaction',
       'deposit'
     )
 
     expect(transaction.getId()).toBe('12345')
     expect(transaction.getAmount()).toBe(100)
-    expect(transaction.getDate()).toBeInstanceOf(Date)
+    expect(transaction.getDate()).toBe('2023-05-01T00:00:00.000Z')
     expect(transaction.getDescription()).toBe('A valid transaction')
     expect(transaction.getType()).toBe('deposit')
   })
