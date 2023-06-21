b5e5e92ffb0d1babfe1fe72b1c5d632cfeac11d6
G2D2 <gabriel.g2d2@gmail.com>
2023-06-20 21:29:59 -0300

    test: 💍 getTotalCards

--- src/domain/Cards/service/get/getListTransactions.ts
+++ src/domain/Cards/service/get/getListTransactions.ts
@@ -1,43 +1,14 @@
 import { TransactionDataTypes } from '@/entity/Transaction/TransactionEntity'
 import { HttpClient } from '@/infra/HttpClient'
 
-function formatCards(arrTransactions: TransactionDataTypes[]) {
-  let totalIncomingTransactions = 0
-  let totalOutgoingTransactions = 0
-  let totalTransactions = 0
-
-  totalTransactions = arrTransactions.reduce((acc, transaction) => {
-    if (transaction.type === 'deposit') {
-      totalIncomingTransactions += transaction.amount
-      return acc + transaction.amount
-    }
-
-    totalOutgoingTransactions -= transaction.amount
-    return acc - transaction.amount
-  }, 0)
-
-  return {
-    totalIncomingTransactions,
-    totalOutgoingTransactions,
-    totalTransactions
-  }
-}
-
 export async function getTotalCards() {
-  const httpClientGet = new HttpClient()
-  const response = await httpClientGet.get<TransactionDataTypes[]>(
-    '/transactions'
-  )
-
-  const {
-    totalTransactions,
-    totalIncomingTransactions,
-    totalOutgoingTransactions
-  } = formatCards(response)
-
-  return {
-    totalIncomingTransactions,
-    totalOutgoingTransactions,
-    totalTransactions
+  try {
+    const httpClient = new HttpClient()
+    const response = await httpClient.get<TransactionDataTypes[]>(
+      '/transactions'
+    )
+    return response
+  } catch (error) {
+    throw new Error('Error to getTotalCards')
   }
 }
