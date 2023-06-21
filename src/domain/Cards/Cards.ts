3092c185648e3341dc31dcebc6297b43239ad898
G2D2 <gabriel.g2d2@gmail.com>
2023-06-20 21:28:37 -0300

    test: 💍 Cards

--- src/domain/Cards/Cards.ts
+++ src/domain/Cards/Cards.ts
@@ -1,8 +1,6 @@
-type GetCardsAPIType = () => Promise<{
-  totalIncomingTransactions: number
-  totalOutgoingTransactions: number
-  totalTransactions: number
-}>
+import { TransactionDataTypes } from '@/entity/Transaction/TransactionEntity'
+
+type GetCardsAPIType = () => Promise<TransactionDataTypes[]>
 
 export class Cards {
   private totalIncomingTransactions = 0
@@ -11,18 +9,46 @@ export class Cards {
 
   constructor(private readonly getCardsAPI: GetCardsAPIType) {}
 
+  formatCards(arrTransactions: TransactionDataTypes[]) {
+    let totalIncomingTransactions = 0
+    let totalOutgoingTransactions = 0
+    let totalTransactions = 0
+
+    totalTransactions = arrTransactions.reduce((acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        totalIncomingTransactions += transaction.amount
+        return acc + transaction.amount
+      }
+
+      totalOutgoingTransactions -= transaction.amount
+      return acc - transaction.amount
+    }, 0)
+
+    return {
+      totalIncomingTransactions,
+      totalOutgoingTransactions,
+      totalTransactions
+    }
+  }
+
   async getCards() {
     const response = await this.getCardsAPI()
     if (!response) {
       this.totalIncomingTransactions = 0
       this.totalOutgoingTransactions = 0
       this.totalTransactions = 0
-      throw new Error('Error getting cards')
+      return
     }
 
-    this.totalIncomingTransactions = response.totalIncomingTransactions
-    this.totalOutgoingTransactions = response.totalOutgoingTransactions
-    this.totalTransactions = response.totalTransactions
+    const {
+      totalIncomingTransactions,
+      totalOutgoingTransactions,
+      totalTransactions
+    } = this.formatCards(response)
+
+    this.totalIncomingTransactions = totalIncomingTransactions
+    this.totalOutgoingTransactions = totalOutgoingTransactions
+    this.totalTransactions = totalTransactions
   }
 
   get totalCards() {
--- src/domain/Cards/Cards.unit.test.ts
+++ src/domain/Cards/Cards.unit.test.ts
@@ -1,50 +1,83 @@
+import { TransactionDataTypes } from '@/entity/Transaction/TransactionEntity'
 import { Cards } from './Cards'
 
-describe('Cards', () => {
-  it('should get cards', async () => {
-    const getCardsAPI = jest.fn().mockResolvedValue({
-      totalIncomingTransactions: 1,
-      totalOutgoingTransactions: 2,
-      totalTransactions: 3
-    })
-
-    const cards = new Cards(getCardsAPI)
-    await cards.getCards()
-
-    expect(cards.totalCards).toEqual({
-      totalIncomingTransactions: 1,
-      totalOutgoingTransactions: 2,
-      totalTransactions: 3
-    })
+describe('Cards - formatCards', () => {
+  it('should be defined', () => {
+    expect(Cards).toBeDefined()
   })
 
-  it('should throw error when get cards', async () => {
-    const getCardsAPI = jest.fn().mockResolvedValue(undefined)
+  it('should create a new Cards', () => {
+    const getCardsAPISpy = jest.fn()
+    const cards = new Cards(getCardsAPISpy)
+    const arrCards: TransactionDataTypes[] = [
+      {
+        id: '1',
+        amount: 100,
+        category: 'category',
+        date: '2021-01-01',
+        name: 'name',
+        type: 'deposit'
+      }
+    ]
 
-    const cards = new Cards(getCardsAPI)
-    await expect(cards.getCards()).rejects.toThrowError('Error getting cards')
+    const result = cards.formatCards(arrCards)
+    const objResult = {
+      totalIncomingTransactions: 100,
+      totalOutgoingTransactions: 0,
+      totalTransactions: 100
+    }
+
+    expect(result).toEqual(objResult)
+  })
+})
+
+describe('Cards - getCards', () => {
+  it('should be defined', () => {
+    expect(Cards).toBeDefined()
+  })
+
+  it('should create a new Cards', async () => {
+    const getCardsAPISpy = jest.fn()
+    const cards = new Cards(getCardsAPISpy)
+    const arrCards: TransactionDataTypes[] = [
+      {
+        id: '1',
+        amount: 100,
+        category: 'category',
+        date: '2021-01-01',
+        name: 'name',
+        type: 'deposit'
+      }
+    ]
+
+    getCardsAPISpy.mockReturnValueOnce(Promise.resolve(arrCards))
+
+    await cards.getCards()
+
+    const objResult = {
+      totalIncomingTransactions: 100,
+      totalOutgoingTransactions: 0,
+      totalTransactions: 100
+    }
+
+    expect(cards.totalCards).toEqual(objResult)
+  })
+
+  it('should return error in the create a new Cards', async () => {
+    const getCardsAPISpy = jest.fn()
+    const cards = new Cards(getCardsAPISpy)
+    getCardsAPISpy.mockReturnValueOnce(
+      Promise.reject(new Error('Error in getCards'))
+    )
+
+    await expect(cards.getCards()).rejects.toThrowError(
+      new Error('Error in getCards')
+    )
 
     expect(cards.totalCards).toEqual({
       totalIncomingTransactions: 0,
       totalOutgoingTransactions: 0,
       totalTransactions: 0
-    })
-  })
-
-  it('should return result the function totalCards', async () => {
-    const getCardsAPI = jest.fn().mockResolvedValue({
-      totalIncomingTransactions: 1,
-      totalOutgoingTransactions: 2,
-      totalTransactions: 3
-    })
-
-    const cards = new Cards(getCardsAPI)
-    await cards.getCards()
-
-    expect(cards.totalCards).toEqual({
-      totalIncomingTransactions: 1,
-      totalOutgoingTransactions: 2,
-      totalTransactions: 3
     })
   })
 })
