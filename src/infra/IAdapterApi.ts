a6f48493949c025cab9c840464c5a8a13ed95fbb
G2D2 <gabriel.g2d2@gmail.com>
2023-07-19 19:35:18 -0300

    refactor: 💡 remover folders

--- src/infra/IAdapterApi.ts
+++
@@ -1,6 +0,0 @@
-export interface IAdapterApi {
-  getApi: (url: string) => Promise<any>
-  postApi: (url: string, data: any) => Promise<any>
-  putApi: (url: string, data: any) => Promise<any>
-  deleteApi: (url: string) => Promise<any>
-}
