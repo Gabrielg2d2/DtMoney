80749ef77b8316d96bd3bfa8b7329dd37d3fb361
G2D2 <gabriel.g2d2@gmail.com>
2023-07-20 16:13:00 -0300

    test: 💍 MainTransaction - listTransactions

---
+++ src/domain/transactions/main.integration.test.ts
@@ -0,0 +1,63 @@
+import { MainTransaction } from './main'
+
+describe('MainTransaction - initial values', () => {
+  test('should return array empty', () => {
+    const mainTransaction = new MainTransaction()
+    const transactions = mainTransaction.allData.listTransactions
+    expect(transactions).toEqual([])
+  })
+
+  test('should return cards all values', () => {
+    const mainTransaction = new MainTransaction()
+    const cards = mainTransaction.allData.cards
+    expect(cards.total).toEqual('R$ 0,00')
+    expect(cards.income).toEqual('R$ 0,00')
+    expect(cards.outcome).toEqual('R$ 0,00')
+  })
+})
+
+describe('MainTransaction - listTransactions', () => {
+  test('should return array empty', async () => {
+    const repository = {
+      list: jest.fn().mockResolvedValueOnce([]),
+      create: jest.fn().mockResolvedValueOnce({}),
+      update: jest.fn().mockResolvedValueOnce({}),
+      delete: jest.fn().mockResolvedValueOnce({})
+    }
+    const mainTransaction = new MainTransaction(repository as any)
+    await mainTransaction.listTransactions()
+    expect(mainTransaction.allData.listTransactions).toEqual([])
+  })
+
+  test('should return a position in array', async () => {
+    const repository = {
+      list: jest.fn().mockResolvedValueOnce([
+        {
+          id: '1',
+          amount: 100,
+          date: '2023-07-01',
+          category: 'category',
+          type: 'deposit',
+          name: 'name'
+        }
+      ]),
+      create: jest.fn().mockResolvedValueOnce({}),
+      update: jest.fn().mockResolvedValueOnce({}),
+      delete: jest.fn().mockResolvedValueOnce({})
+    }
+    const mainTransaction = new MainTransaction(repository as any)
+    await mainTransaction.listTransactions()
+    expect(mainTransaction.allData.listTransactions).toEqual([
+      {
+        id: '1',
+        amount: 100,
+        amountFormatted: 'R$ 1,00',
+        date: '2023-07-01',
+        dateFormatted: '01/07/2023',
+        category: 'category',
+        type: 'deposit',
+        name: 'name'
+      }
+    ])
+  })
+})
